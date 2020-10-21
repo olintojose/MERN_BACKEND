b68d301702ca1ef570e7c793dbf6f017332f98fc
Olinto <olintojose@gmail.com>
2020-10-21 18:28:46 -0300

    ajustes para deploy

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar Rutas
 
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
 })
 
 //Arrancar el servidor (app)
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });

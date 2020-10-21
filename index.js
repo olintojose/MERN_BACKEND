2dea8a9b045a09e634868149d4db640b5784fa09
Olinto <olintojose@gmail.com>
2020-10-21 18:51:59 -0300

    ajustes para deploy2

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.get('/', (req, res) => {
 
 //Arrancar el servidor (app)
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
